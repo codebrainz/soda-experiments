1483192ffc4e4111c6e7b6329d9f40878a5f628d
Matthew Brush <mbrush@codebrainz.ca>
2014-04-06 00:09:29 -0700

    Cleanup lexer tests a bit

--- test_lexer.cc
+++ test_lexer.cc
@@ -5,67 +5,37 @@
 
 using namespace Soda;
 
+#define CHECK(inp_text, tok, str_check)      \
+	do {                                     \
+		std::string inp_text_(inp_text);     \
+		inp_text_+=" ";                      \
+		ss << inp_text_;                     \
+		k = lex.next();                      \
+		assert(k == Token::tok);             \
+		assert(lex.token.text == str_check); \
+	} while (0)
+
 int main()
 {
 	std::stringstream ss;
 	Token::Kind k;
 
-	ss << "int double 0x00ff 0b1001 0755 123.456 .456 123. 0123. 'a' '\\'' "
-	      "// some comment\n /* a multi\n comment */";
+	ss << " "; // prime input stream;
 
 	Lexer lex(ss);
 
-	k = lex.next();
-	assert(k == Token::IDENT);
-	assert(lex.token.text == U"int");
-
-	k = lex.next();
-	assert(k == Token::IDENT);
-	assert(lex.token.text == U"double");
-
-	k = lex.next();
-	assert(k == Token::HEX_ICONST);
-	assert(lex.token.text == U"00ff");
-
-	k = lex.next();
-	assert(k == Token::BIN_ICONST);
-	assert(lex.token.text == U"1001");
-
-	k = lex.next();
-	assert(k == Token::OCT_ICONST);
-	assert(lex.token.text == U"0755");
-
-	k = lex.next();
-	assert(k == Token::FCONST);
-	assert(lex.token.text == U"123.456");
-
-	k = lex.next();
-	assert(k == Token::FCONST);
-	assert(lex.token.text == U".456");
-
-	k = lex.next();
-	assert(k == Token::FCONST);
-	assert(lex.token.text == U"123.");
-
-	k = lex.next();
-	assert(k == Token::FCONST);
-	assert(lex.token.text == U"0123.");
-
-	k = lex.next();
-	assert(k == Token::CHAR_ICONST);
-	assert(lex.token.text == U"a");
-
-	k = lex.next();
-	assert(k == Token::CHAR_ICONST);
-	assert(lex.token.text == U"\\'");
-
-	k = lex.next();
-	assert(k == Token::COMMENT);
-	assert(lex.token.text == U" some comment");
-
-	k = lex.next();
-	assert(k == Token::COMMENT);
-	assert(lex.token.text == U" a multi\n comment ");
+	CHECK("int ", IDENT, U"int");
+	CHECK("double ", IDENT, U"double");
+	CHECK("0x00ff ", HEX_ICONST, U"00ff");
+	CHECK("0b1001 ", BIN_ICONST, U"1001");
+	CHECK("0755 ", OCT_ICONST, U"0755");
+	CHECK("123.456 ", FCONST, U"123.456");
+	CHECK(".456", FCONST, U".456");
+	CHECK("123.", FCONST, U"123.");
+	CHECK("'a' ", CHAR_ICONST, U"a");
+	CHECK("'\\''", CHAR_ICONST, U"\\'");
+	CHECK("// some comment\n ", COMMENT, U" some comment");
+	CHECK("/* a multi\n comment */", COMMENT, U" a multi\n comment ");
 
 	return 0;
 }
